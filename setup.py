a07f5fe545f5299c22ca887d4ff970fd90be1b6a
Nathan Malkin <nmalkin@cs.berkeley.edu>
2022-04-02 22:29:26 -0700

    Add long description for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="kupper_hafner",
     version="0.1.0",
     description="Kupper-Hafner inter-rater agreement calculation library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='http://github.com/nmalkin/kupper_hafner',
     author='nmalkin',
     license='BSD',
